4d2ff110fb592fff7ffb31d68f6703992a29430c
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-11-22 12:16:43 -0300

    #1 Adicionando função create, que cria um usuário e adiciona ao banco de dados, por meio do método POST.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,43 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async index(request, response) {
+    
+    async create(request, response){
+       
+        const {name, email, senha} = request.body;
+
         try {
+
+            const emailExists = await connection('usuarios').where('email', email).select('email').first();
+            
+            if (emailExists) {
+                
+                return response.status(400).send('Este email ja esta em uso!');
+            }
+                await connection('usuarios').insert({
+                    name,
+                    email,
+                    senha
+                });
+                
+                return response.status(201).send("Usuário criado com sucesso!")
+            }
+            
+            catch (error) {
+                return response.status(400).send(error);
+            }
+         
+    },
+     
+    async index(request, response) {
+        
+        try {
+
             const users = await connection('usuarios').select('*');
             return response.send({users});
         
         } catch (error) {
+            
             return response.status(400).send(error);
         }
         
