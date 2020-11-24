f7ba9d766bc1a7a1f91fee83e89176132c0d800c
Bispo <Juniorbispo515@gmail.com>
2020-11-24 00:26:20 -0300

    #2 Adicionando rota de alteração de email ao routes e adaptando o path das rotas.

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
 const SessionController = require('./controllers/SessionController');
+const ProfileController = require('./controllers/ProfileController');
+
 
 routes.get('/users', UserController.index);
 routes.post('/user', UserController.create);
 routes.delete('/user/:id', UserController.remove);
-routes.put('/user', UserController.editName);
+routes.put('/profile/edit_name', ProfileController.editName);
+routes.put('/profile/edit_email', ProfileController.editEmail);
 routes.post('/login', SessionController.login);
 
 module.exports = routes;
