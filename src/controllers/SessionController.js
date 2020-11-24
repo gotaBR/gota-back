8059b8d0a994421447ba7e726858353e3806f120
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-11-24 14:47:04 -0300

    HOTFIX corrigindo erro de retorno no login.

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,26 +4,25 @@ const connection = require('../database/connection');
 module.exports = {
   async login(request, response) {
     const { email, senha } = request.body;
+    console.log({ email, senha });
     let user;
     try {
       [user] = await connection('usuarios').where('email', email).select('*');
-    } catch (error) {
-      return response.status(400).send(error);
-    }
+      console.log(user);
 
-    if (!user) {
-      return response.status(404).send('Could not find this email.');
-    }
-
-    bcrypt.compare(senha, user.senha, (error, same) => {
-      if (same) {
-        const { id } = user;
-        return response.status(200).send({ id, message: 'Usuário logado' });
+      if (!user) {
+        return response.status(404).send('Could not find this email.');
       }
+      return bcrypt.compare(senha, user.senha, (error, same) => {
+        if (same) {
+          const { id } = user;
+          return response.status(200).send({ id, message: 'Usuário logado' });
+        }
 
-      return response.status(401).send({ error, message: 'Senha errada' });
-    });
-
-    return response.status(400).send('Erro de login.');
+        return response.status(401).send({ error, message: 'Senha errada' });
+      });
+    } catch (error) {
+      return response.status(400).send(error.message);
+    }
   },
 };
