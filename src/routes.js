3c5c7efdd32207e3537bfc814e0a9178f3dd8974
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-11-24 21:31:00 -0300

    #2 Adicionando rota de alteração de senha do usuário por meio de requisição PUT ao routes.

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ const UserController = require('./controllers/UserController');
 const SessionController = require('./controllers/SessionController');
 const ProfileController = require('./controllers/ProfileController');
 
-
 routes.get('/users', UserController.index);
 routes.post('/user', UserController.create);
 routes.delete('/user/:id', UserController.remove);
 routes.put('/profile/edit_name', ProfileController.editName);
 routes.put('/profile/edit_email', ProfileController.editEmail);
+routes.put('/profile/new_password', ProfileController.createNewPassword);
 routes.post('/login', SessionController.login);
 
 module.exports = routes;
