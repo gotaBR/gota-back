3dceb86b785c4cbe7d281984ea02113f26b9e44e
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-11-27 14:35:18 -0300

    #3 Adicionando rota de registro de contas de água e esgoto.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const UserController = require('./controllers/UserController');
 const SessionController = require('./controllers/SessionController');
 const ProfileController = require('./controllers/ProfileController');
+const BillsController = require('./controllers/BillsController');
 
 /* routes.get('/users', UserController.index); */
 routes.post('/user', UserController.create);
@@ -13,5 +14,7 @@ routes.put('/profile/edit_name', ProfileController.editName);
 routes.put('/profile/edit_email', ProfileController.editEmail);
 routes.put('/profile/new_password', ProfileController.createNewPassword);
 routes.post('/login', SessionController.login);
+
+routes.post('/profile/new_bill', BillsController.create);
 
 module.exports = routes;
