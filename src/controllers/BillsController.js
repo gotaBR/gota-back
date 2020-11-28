da7450db6e18feb372dc33266e5dbe9e67736a2e
Bispo <Juniorbispo515@gmail.com>
2020-11-28 12:46:01 -0300

    Adiçao da funcao que modifica o status da conta de agua de um usuario

--- src/controllers/BillsController.js
+++ src/controllers/BillsController.js
@@ -53,4 +53,22 @@ module.exports = {
       return response.status(400).send(error.message);
     }
   },
+
+  async update(request, response) {
+    const userId = request.headers.authorization;
+    const { id } = request.headers;
+    const { newStatus } = request.body;
+
+    try {
+      const bill = await connection('contas').where('id', id).where('user_id', userId).update({ status: newStatus });
+
+      if (!bill) {
+        return response.status(400).send('Conta não encontrada.');
+      }
+
+      return response.status(200).send('Status atualizado com sucesso.');
+    } catch (error) {
+      return response.status(400).send(error.message);
+    }
+  },
 };
